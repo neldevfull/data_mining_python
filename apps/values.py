3d4efb91f701be6184a7710762a6874380b7f021
neldevfull <nfrugeri@gmail.com>
2016-08-06 21:38:08 +0000

    Implemented method strptime of the datetime

--- apps/values.py
+++ apps/values.py
@@ -2,34 +2,24 @@
 
 import sys
 from decimal import Decimal
-from datetime import date
+from datetime import datetime
 
 
 class FinancialExecution:
     def __init__(self, date_ini, date_end):
+        self._total   = Decimal('0')
         self.date_ini = date_ini
         self.date_end = date_end
 
-        dateini = date_ini.split('/')
-        dateend = date_end.split('/')
-
-        self._total = Decimal('0')
-        self.start_date = date(int(dateini[2]), int(dateini[1]), int(dateini[0]))
-        self.end_date   = date(int(dateend[2]), int(dateend[1]), int(dateend[0]))
-
-    def _mount_date(self, str_date):
-            year  = int(str_date.split('/')[2])
-            month = int(str_date.split('/')[1])
-            day   = int(str_date.split('/')[0])
-
-            return date(year, month, day)
+        self.start_date = datetime.strptime(self.date_ini, '%d/%m/%Y')
+        self.end_date   = datetime.strptime(self.date_end, '%d/%m/%Y')
 
     def total(self):
         with open('infra/saida/data/data/ExecucaoFinanceira.csv', 'r') as data:
             for line in data:
                 try:
                     info = line.split(';')
-                    d_date = self._mount_date(info[7]) # date
+                    d_date = datetime.strptime(info[7], '%d/%m/%Y') # date
 
                     if (d_date >= self.start_date and
                         d_date <= self.end_date):
