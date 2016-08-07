a2bb3bea7bc6e29c7bb1889179a95acfa97de831
neldevfull <nfrugeri@gmail.com>
2016-08-07 18:52:18 +0000

    Implemented companies that received values between one bilion and one thousand reais

--- apps/values.py
+++ apps/values.py
@@ -6,6 +6,40 @@ from datetime import datetime, timedelta
 
 
 class FinancialExecution:
+    def get_id_and_value(self, info, lower):
+        value = Decimal(info[5])
+
+        if value > lower:
+            return info[2], value
+        return None, Decimal(0)
+
+    def companies_received(self):
+        all_companies = set()
+        intervals = [
+            (Decimal('1000000000'), set()),
+            (Decimal('500000000'), set()),
+            (Decimal('100000000'), set()),
+            (Decimal('10000000'), set()),
+            (Decimal('1000000'), set()),
+            (Decimal('1000'), set())
+        ]
+
+        for lower, companies in intervals:
+            data = open('infra/saida/data/data/ExecucaoFinanceira.csv', 'r')
+            for line in data:
+                info = line.strip().split(';')
+                company_id, contract_value = self.get_id_and_value(info, lower)
+
+                if company_id and not company_id in all_companies:
+                    companies.add(company_id)
+                    all_companies.add(company_id)
+
+        data.close()
+
+        for lower, companies in intervals:
+            print("{} companies received more than {}".format(len(companies), lower))
+        print("{} companies total".format(len(all_companies)))
+
     def total(self, date_ini, date_end):
         total = Decimal('0')
         start_date = datetime.strptime(date_ini, '%d/%m/%Y')
